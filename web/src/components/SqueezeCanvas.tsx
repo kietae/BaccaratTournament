343b44a8caf3a497a0f23cd98210b119488c38cb
kitaepark <kitae.park@inspirekorea.com>
2026-08-27 15:05:36 +0900

    Move vertical thumbs onto the folded card

--- web/src/components/SqueezeCanvas.tsx
+++ web/src/components/SqueezeCanvas.tsx
@@ -321,8 +321,8 @@ export default function SqueezeCanvas(props: SqueezeCanvasProps) {
         const shortEdgeInset = vertical ? 0 : thumbWidth * 0.3;
         const thumbTip = edge === 'left' ? { x: tipNormal, y: tangent }
           : edge === 'right' ? { x: width - tipNormal, y: tangent }
-            : edge === 'top' ? { x: tangent, y: tipNormal + shortEdgeInset }
-              : { x: tangent, y: height - tipNormal - shortEdgeInset };
+            : edge === 'top' ? { x: tangent, y: tipNormal - shortEdgeInset }
+              : { x: tangent, y: height - tipNormal + shortEdgeInset };
         ctx.save();
         ctx.translate(thumbTip.x, thumbTip.y);
         ctx.rotate(thumbAngle);
